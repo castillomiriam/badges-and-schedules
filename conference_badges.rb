1ce831d04b4052ee06418608d13a8b85f547d561
Miriam Castillo <castillo.m.mc@gmail.com>
2022-11-16 18:18:23 -0700

    commit tree 41f4c408e1275a4d972ba39de9f2f4c51739692c

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
 # Write your code here.
+def badge_maker(name)
+
+    return "Hello, my name is #{name}."
+
+
+end
+
+
+
+
+def batch_badge_creator(names)
+
+    names.map {|name| badge_maker(name)}
+end
+
+ def assign_rooms(speaker)
+
+    room_assignment = []
+        speaker.each_with_index {|name,room| room_assignment.push("Hello, #{name}! You'll be assigned to room #{room +1}!")}
+         room_assignment
+end
+
+def printer(attendes)
+   batch_badge_creator(attendes).each {|name| puts name}
+   assign_rooms(attendes).each{|room| puts room}
+
+end
